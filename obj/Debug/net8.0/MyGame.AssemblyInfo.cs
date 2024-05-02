7c3eabddd056aa78f9ae81b0a51f967f0c45eb83
Zusk <kingofbubbles@gmail.com>
2024-05-02 11:58:15 -0400

    Credits for music file

--- obj/Debug/net8.0/MyGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dac2eea058571373dbab3db35ca5eca21bd71ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f504304171556b93b07e6e1dfb350fa4957ffd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
